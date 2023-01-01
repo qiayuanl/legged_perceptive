01e9699c575f225aa81d38a92606cc0b4e8c6e1d
qiayuan <liaoqiayuan@gmail.com>
2023-01-01 15:52:03 +0800

    Add FootPlacementConstraint

---
+++ sdf_interface/include/sdf_interface/FootPlacementConstraint.h
@@ -0,0 +1,45 @@
+//
+// Created by qiayuan on 23-1-1.
+//
+
+#pragma once
+
+#include <ocs2_core/constraint/StateConstraint.h>
+#include <ocs2_legged_robot/reference_manager/SwitchedModelReferenceManager.h>
+#include <ocs2_robotic_tools/end_effector/EndEffectorKinematics.h>
+
+namespace legged {
+
+using namespace ocs2;
+using namespace legged_robot;
+
+class FootPlacementConstraint final : public StateConstraint {
+ public:
+  /**
+   * Constructor
+   * @param [in] referenceManager : Switched model ReferenceManager
+   * @param [in] endEffectorKinematics: The kinematic interface to the target end-effector.
+   * @param [in] contactPointIndex : The 3 DoF contact index.
+   */
+  FootPlacementConstraint(const SwitchedModelReferenceManager& referenceManager,
+                          const EndEffectorKinematics<scalar_t>& endEffectorKinematics, size_t contactPointIndex);
+
+  ~FootPlacementConstraint() override = default;
+  FootPlacementConstraint* clone() const override { return new FootPlacementConstraint(*this); }
+
+  bool isActive(scalar_t time) const override;
+  size_t getNumConstraints(scalar_t time) const override { return 1; }
+  vector_t getValue(scalar_t time, const vector_t& state, const PreComputation& preComp) const override;
+  VectorFunctionLinearApproximation getLinearApproximation(scalar_t time, const vector_t& state,
+                                                           const PreComputation& preComp) const override;
+
+ private:
+  FootPlacementConstraint(const FootPlacementConstraint& rhs);
+
+  const SwitchedModelReferenceManager* referenceManagerPtr_;
+  std::unique_ptr<EndEffectorKinematics<scalar_t>> endEffectorKinematicsPtr_;
+
+  const size_t contactPointIndex_;
+};
+
+}  // namespace legged
---
+++ sdf_interface/src/FootPlacementConstraint.cpp
@@ -0,0 +1,35 @@
+//
+// Created by qiayuan on 23-1-1.
+//
+
+#include "sdf_interface/FootPlacementConstraint.h"
+
+namespace legged {
+FootPlacementConstraint::FootPlacementConstraint(const SwitchedModelReferenceManager& referenceManager,
+                                                 const EndEffectorKinematics<scalar_t>& endEffectorKinematics, size_t contactPointIndex)
+    : StateConstraint(ConstraintOrder::Linear),
+      referenceManagerPtr_(&referenceManager),
+      endEffectorKinematicsPtr_(endEffectorKinematics.clone()),
+      contactPointIndex_(contactPointIndex) {}
+
+FootPlacementConstraint::FootPlacementConstraint(const FootPlacementConstraint& rhs)
+    : StateConstraint(ConstraintOrder::Linear),
+      referenceManagerPtr_(rhs.referenceManagerPtr_),
+      endEffectorKinematicsPtr_(rhs.endEffectorKinematicsPtr_->clone()),
+      contactPointIndex_(rhs.contactPointIndex_) {}
+
+bool FootPlacementConstraint::isActive(scalar_t time) const {
+  bool contact = referenceManagerPtr_->getContactFlags(time)[contactPointIndex_];
+  return !contact;
+}
+
+vector_t FootPlacementConstraint::getValue(scalar_t time, const vector_t& state, const PreComputation& preComp) const {
+  return ocs2::vector_t();
+}
+
+VectorFunctionLinearApproximation FootPlacementConstraint::getLinearApproximation(scalar_t time, const vector_t& state,
+                                                                                  const PreComputation& preComp) const {
+  return StateConstraint::getLinearApproximation(time, state, preComp);
+}
+
+}  // namespace legged
