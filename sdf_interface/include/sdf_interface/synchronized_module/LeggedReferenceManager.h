98a4f17564499a42c1383f33ab035dbe14871453
qiayuan <liaoqiayuan@gmail.com>
2023-01-04 02:41:17 +0800

    Add LeggedReferenceManager

---
+++ sdf_interface/include/sdf_interface/synchronized_module/LeggedReferenceManager.h
@@ -0,0 +1,31 @@
+//
+// Created by qiayuan on 23-1-3.
+//
+#pragma once
+
+#include <memory>
+
+#include "sdf_interface/ConvexRegionSelector.h"
+
+#include <ocs2_legged_robot/reference_manager/SwitchedModelReferenceManager.h>
+
+namespace legged {
+
+using namespace ocs2;
+using namespace legged_robot;
+
+class LeggedReferenceManager : public SwitchedModelReferenceManager {
+ public:
+  LeggedReferenceManager(std::shared_ptr<GaitSchedule> gaitSchedulePtr, std::shared_ptr<SwingTrajectoryPlanner> swingTrajectoryPtr,
+                         std::shared_ptr<ConvexRegionSelector> convexRegionSelectorPtr);
+
+  const std::shared_ptr<ConvexRegionSelector>& getConvexRegionSelectorPtr() { return convexRegionSelectorPtr_; }
+
+ private:
+  void modifyReferences(scalar_t initTime, scalar_t finalTime, const vector_t& initState, TargetTrajectories& targetTrajectories,
+                        ModeSchedule& modeSchedule) override;
+
+  std::shared_ptr<ConvexRegionSelector> convexRegionSelectorPtr_;
+};
+
+}  // namespace legged
---
+++ sdf_interface/src/synchronized_module/LeggedReferenceManager.cpp
@@ -0,0 +1,26 @@
+//
+// Created by qiayuan on 23-1-3.
+//
+#include <utility>
+
+#include "sdf_interface/synchronized_module/LeggedReferenceManager.h"
+
+namespace legged {
+
+LeggedReferenceManager::LeggedReferenceManager(std::shared_ptr<GaitSchedule> gaitSchedulePtr,
+                                               std::shared_ptr<SwingTrajectoryPlanner> swingTrajectoryPtr,
+                                               std::shared_ptr<ConvexRegionSelector> convexRegionSelectorPtr)
+    : SwitchedModelReferenceManager(std::move(gaitSchedulePtr), std::move(swingTrajectoryPtr)),
+      convexRegionSelectorPtr_(std::move(convexRegionSelectorPtr)) {}
+
+void LeggedReferenceManager::modifyReferences(scalar_t initTime, scalar_t finalTime, const vector_t& initState,
+                                              TargetTrajectories& targetTrajectories, ModeSchedule& modeSchedule) {
+  const auto timeHorizon = finalTime - initTime;
+  modeSchedule = getGaitSchedule()->getModeSchedule(initTime - timeHorizon, finalTime + timeHorizon);
+  const scalar_t terrainHeight = 0.0;
+  getSwingTrajectoryPlanner()->update(modeSchedule, terrainHeight);
+
+  convexRegionSelectorPtr_->update(modeSchedule, initState, targetTrajectories);
+}
+
+}  // namespace legged
